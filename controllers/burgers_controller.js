56417a34b88bd3743539355086b569caedb95f56
Matthew Vanni <mvanni4994@gmail.com>
2021-04-10 11:57:21 -0400

    added to controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,51 @@
 const express = require('express');
 const burgers = require('../models/burger');
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+      const hbsObject = {
+        burger: data,
+      };
+      console.log(hbsObject);
+      res.render('index', hbsObject);
+    });
+  });
+
+  router.post('/api/burgers', (req, res) => {
+    burger.insertOne(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
+      res.json({ id: result.insertId });
+    });
+  });
+
+  router.put('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    console.log('condition', condition);
+  
+    burger.updateOne(
+      {
+        devoured: true,
+      },
+      condition,
+      (result) => {
+        if (result.changedRows === 0) {
+          return res.status(404).end();
+        }
+        res.status(200).end();
+      }
+    );
+  });
+  
+  router.delete('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    burger.delete(condition, (result) => {
+      if (result.affectedRows === 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    });
+  });
+  
+module.exports = router
